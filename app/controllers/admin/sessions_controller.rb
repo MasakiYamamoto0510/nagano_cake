b045d2b1258e0d68dcc16f121d1ba00e1ab6219e
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-02-08 03:39:17 +0000

    Add homes view to admin

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -26,6 +26,10 @@ class Admin::SessionsController < Devise::SessionsController
   # end
 
   def after_sign_in_path_for(resource)
-    
+    admin_homes_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    new_admin_session_path # ログアウト後にリダイレクトするパス
   end
 end
