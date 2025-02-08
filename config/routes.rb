a7c36a3bea306c162ab03406fdbea047f7e8b9e3
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2025-02-08 03:39:55 +0000

    Add homes view to admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
+  namespace :admin do
+   get 'homes', to: 'homes#top'
+  end
+
   devise_for :customers, skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
